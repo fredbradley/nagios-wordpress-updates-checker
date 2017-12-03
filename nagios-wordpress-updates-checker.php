9336d8ffd6c41ef247d4763ac5de055674eb58f2
Fred Bradley <hello@fredbradley.co.uk>
2017-12-03 21:28:07 +0000

    Version Bump

--- nagios-wordpress-updates-checker.php
+++ nagios-wordpress-updates-checker.php
@@ -3,7 +3,7 @@
 Plugin Name: Nagios Wordpress Updates Checker
 Description: Nagios Wordpress Updates Checker
 Author: Fred Bradley
-Version: 1.9.13
+Version: 1.9.14
 Author URI: http://fred.im/
 Network: true
 */
